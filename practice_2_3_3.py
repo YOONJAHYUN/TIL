6ac6b1cdc85e0c661518dcefe3352a979eca104f
윤자현 <bajh323@naver.com>
2023-01-16 14:21:42 +0900

    주석

--- practice_2_3_3.py
+++ practice_2_3_3.py
@@ -3,10 +3,13 @@ test = map(int,input().split())
 
 i = 0
 for i in test:
+    # 사용자가 처음 입력한 값이 범위가 됨
     if i >= int(a) and i <= int(b):
         print('Nothing to report')
+    # -999입력시 실험 중지    
     elif i == -999:
         break
+    # 입력값이 범위 초과되면 경고!
     else :
         print('Alert!')
         break
